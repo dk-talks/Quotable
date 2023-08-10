47501dcab7ca7bf04c8b9f00a002d7f68c88f3b5
dk_talks <dksbansur8502@gmail.com>
2023-08-10 23:45:59 +0530

    sign up window created

--- Quotable/ViewController.swift
+++ Quotable/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var btnNext: UIButton!
+    @IBOutlet weak var textMobileNo: UITextField!
+    @IBOutlet weak var btnChangeCountry: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnNextTapped(_ sender: Any) {
+    }
+    
+    @IBAction func btnChangeCountryTapped(_ sender: Any) {
+    }
 }
 
